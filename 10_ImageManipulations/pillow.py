c29374b3db038e88635aadb69bb08b88955fbadf
CarlosHenriqueMatos <86526136+CarlosHenriqueMatos@users.noreply.github.com>
2023-02-01 18:56:38 -0300

    Add files via upload

--- 10_ImageManipulations/pillow.py
+++ 10_ImageManipulations/pillow.py
@@ -2,7 +2,10 @@
 
 Obs: was made some modifications like functions to dont use comments
 '''
-from PIL import Image, ImageColor
+from PIL import Image, ImageColor, ImageEnhance, ImageFilter
+import cv2
+import numpy
+from matplotlib import cm
 
 def ImportImage(way, name):
     imported_image = Image.open(way+name)
@@ -38,20 +41,106 @@ def ResizeImage(image, length, width):
 def DoubleResolution(scale_factor, image):
     return image.resize((image.size[0] * scale_factor, image.size[1] * scale_factor))
     
+def EnhanceImage(image, value):
+    
+    color_enhancer = ImageEnhance.Color(image)
+    color_image = color_enhancer.enhance(value)
+    
+    contrast_enhancer = ImageEnhance.Contrast(image)
+    contrast_image = contrast_enhancer.enhance(value)
+    
+    brightness_enhancer = ImageEnhance.Brightness(image)
+    brightness_image = brightness_enhancer.enhance(value)
+    
+    sharpness_enhancer = ImageEnhance.Sharpness(image)
+    sharpness_image = sharpness_enhancer.enhance(value)
+
+    
+    #InformationsImage(color_image)
+    #InformationsImage(contrast_image)
+    #InformationsImage(brightness_image)
+    #InformationsImage(sharpness_image)
+
+    
+def FImageFilter(image):
+    
+    image_blur = image.filter(ImageFilter.BLUR)
+    image_contour = image.filter(ImageFilter.CONTOUR)
+    image_detail = image.filter(ImageFilter.DETAIL)
+    image_edge = image.filter(ImageFilter.EDGE_ENHANCE)
+    image_edge_more = image.filter(ImageFilter.EDGE_ENHANCE_MORE)
+    image_find_edges = image.filter(ImageFilter.FIND_EDGES)
+    image_emboss = image.filter(ImageFilter.EMBOSS)
+    image_sharp = image.filter(ImageFilter.SHARPEN)
+    image_smooth = image.filter(ImageFilter.SMOOTH())
+    image_smooth_more = image.filter(ImageFilter.SMOOTH_MORE)
+    
+    
+    image_contour_edges = image_edge.filter(ImageFilter.CONTOUR)
+    image_edges_emboss_countour = image_contour_edges.filter(ImageFilter.FIND_EDGES)
+    image_edges_emboss_countourBW = image_edges_emboss_countour.convert('LA')
+    #image_edges_emboss_countourBW.show()
+    return image_edges_emboss_countourBW
+'''    
+    image_blur.show()
+    image_contour.show()
+    image_detail.show()
+    image_edge.show()
+    image_edge_more.show()
+    image_find_edges.show()
+    image_emboss.show()
+    image_sharp.show()
+    image_smooth.show()
+    image_smooth_more.show()
+'''
+
+def ImageFilter2(image):
+    image_filtered_min = image.filter(ImageFilter.MinFilter(size = 5))
+    image_filtered_median = image.filter(ImageFilter.MedianFilter(size = 5))
+    image_filtered_max = image.filter(ImageFilter.MaxFilter(size = 5))
+    image_filtered_min.show()
+    image_filtered_median.show()
+    image_filtered_max.show()
+    
+def ImageFilter3(image):
+    image_boxblur = image.filter(ImageFilter.BoxBlur())
+    
+def BlackOrWhite(image):
+    print(image.shape)
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            for k in range(2):
+                if image[i][j][k] > 170:
+                    image[i][j][k] = 255
+                else:
+                    image[i][j][k] = 0
+    return image
+
+def ContourBlackAndWhite(image):
+    image_grayscale = FImageFilter(image)
+    image_grayscale = numpy.array(image_grayscale)
+    image = BlackOrWhite(image_grayscale)
+    PIL_image = Image.fromarray(numpy.uint8(image)).convert('L')
+    PIL_image = Image.fromarray(image.astype('uint8'))
+    
+    return PIL_image
+
+def BoxGaussianUsharpFilters(image):
+    image_boxblur = image.filter(ImageFilter.BoxBlur(radius = 4))
+    image_gaussblur = image.filter(ImageFilter.GaussianBlur(radius = 4))
+    image_unsharp = image.filter(ImageFilter.UnsharpMask(radius = 4))
+    image_boxblur.show()
+    image_gaussblur.show()
+    image_unsharp.show()
     
 def main():
+    
     path = "/home/carlos/Documents/SwapDS/dataset/"
     name_image = "Rafd090_01_Caucasian_female_angry_left.jpg"
-    
+
     image = ImportImage(path,name_image)
-    
-    InformationsImage(image)
-    
-    image_double_resolution = DoubleResolution(2, image)
-    SaveAndPrintImage(image_double_resolution, path, "fifithresolution.jpg")
-    InformationsImage(image_double_resolution)
-    
-    
+    print(image.mode)
+    BoxGaussianUsharpFilters(image)
     
 if __name__ == "__main__":
     main()
